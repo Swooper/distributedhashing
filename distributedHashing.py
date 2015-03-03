fc93170327bba0fd895d64bcddfb1e37547220f9
Swooper <bluecloak@gmail.com>
2015-03-03 16:41:07 +0000

    Populating nodes, initialising servers

--- distributedHashing.py
+++ distributedHashing.py
@@ -6,14 +6,44 @@ n1 = []
 n2 = []
 n3 = []
 
-def start(s, e):
-        while(s>0):
-                addServer()
-                s--
-        #e = extent
+extent = 0
+serverIds = []
+servers = []
+serverCount = 0
 
-def addServer():
-        pass
+class Server(object):
+
+        def __init__(self, serverId, first, last):
+                self.serverId = serverId
+                self.first = first
+                self.last = last
+                
+def start(s, e):
+        extent = e
+        populateN()
+        addServer(s)
+        
+def populateN():
+        int i = 0
+        for i in range(extent):
+                n1.append("")
+                n2.append("")
+                n3.append("")
+                
+def addServer(s):
+        partitionSize = extent / s
+        serverCount += s
+        while s>0:
+                i = 0
+                while i in serverIds:
+                        i+=1
+                first = (serverCount-s)*partitionSize
+                last = first+partitionSize-1
+                servers.append(new Server(i, first, last))
+                serverIds.append(i)
+                s-=1
 
 def killServer(serverId):
         pass
+
+start(10, 10000)
