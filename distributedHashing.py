d53f2f1dc991167a25daa8a763035b888608d9cc
TryggviThor <tryggvithorg@gmail.com>
2015-03-08 14:24:53 +0000

    Rewritten, Server object not used yet, do we need it?

--- distributedHashing.py
+++ distributedHashing.py
@@ -7,16 +7,17 @@ import random
 
 # A Server object, with a serverId and a boolean assigned value
 class Server():
-        def __init__(self, serverId, assigned):
+        def __init__(self, serverId, start, end):
                 self.serverId = serverId
-                self.assigned = assigned
+                self.start = start
+                self.end = end
                 pass
 
 # Distributed hashing
 class DHash():
         def __init__(self):
 
-                # constants
+                # constants for the "update" function
                 self.million = 1000000
                 self.tenperc = int(self.million*0.1)
                 self.oneperc = int(self.million*0.01)
@@ -24,13 +25,13 @@ class DHash():
                 # S, Servers, systems
                 # E, Extents
                 # N, Reflections, duplicates, replicas
-                self.S = 0 # start with zero, change this as we add more
-                self.E = 100
-                self.N = 3
-                
-                self.nextS = 0
+                # Start at zero, change this as we add more
+                self.S = 0 
+                self.E = 50
+                self.N = 0
 
-                self.Replicas = self.fillReplicas(self.N)
+                self.Servers = []
+                self.Replicas = []
                 pass
 
         # Fill the Replicas with None and False values,
@@ -38,9 +39,9 @@ class DHash():
         def fillReplicas(self, N):
                 Replicas = [[] for n in range(N)]
 
-                for i in range(self.N):
+                for n in range(self.N):
                         for j in range(self.E):
-                                Replicas[i].append(Server(None, False))
+                                Replicas[n].append(None)
                                 pass
                         pass
 
@@ -50,80 +51,84 @@ class DHash():
         # the index in every Replica for a million (using) simulated writes
         def update(self):
                 counts = [0 for s in range(self.S)]
-                
-                conflicts = 0
-                ccounts = [0 for s in range(self.S)]
-                cplaces = []
-
                 using = self.oneperc
+
 
                 for i in range(using):
                         randi =  random.randint(0, self.E-1)
 
-                        for Replica in self.Replicas:
-                                counts[Replica[randi].serverId] += 1
-                                pass
-
+                        # for Replica in self.Replicas:
+                        #         counts[Replica[randi].serverId] += 1
+                        #         pass
                         
                         pass
 
                 # Change the format to percentages
-                for c in range(self.S):
-                        counts[c] = round((float(counts[c])/(using*3))*100, 2)
-                        pass
+                #       round((float(counts[c])/(using*3))*100, 2)
 
-                return str(counts)+", "+str(sum(counts))+"\nConflicts: "+str(conflicts)+", "+str(ccounts)+", "+str(cplaces)
+                return counts
 
         # Add snew into Replicas
         #   for each Replica:
-        #               choose a random place, make that place "assigned"
-        #               run down the Replica and add the server handle until it reaches the next server in that Replica 
-        #                                                                                                          or the same server value is already at that place in other Replicas
         def addToReplicas(self, snew):
-                randis = [random.randint(0, self.E-1) for ri in range(self.N)]
+                randis = [random.randint(0, self.E-1) for n in range(self.N)]
 
-                # Randomed index can't be the same as the start of an already-there server
-                # OR the new serverId can't be the same as the one already-there
-                for ri in range(self.N):
-                        for n in self.Replicas:
-                                while snew == n[randis[ri]].serverId:
-                                        randis[ri] = random.randint(0, self.E-1)
+                # Randomed index can't be the same as the server that's already there
+                for n in range(self.N):
+                        for rep in self.Replicas:
+                                while snew == rep[randis[n]]:
+                                        randis[n] = random.randint(0, self.E-1)
                                         pass
                                 pass
                         pass
                 
-                
                 # Use the randomized index for add the server into the replicas
-                start = True
+                # Going from randis[n] down (and wrap-around) to randis[n+1]
                 for e in reversed(range(self.E)):
                         for n in range(self.N):
-                                self.addToReplica(n, snew, (randis[n]-e)%self.E, start)
-                                pass
+                                nextIndex = (e+randis[n]+1)%self.E
+
+                                # Check if snew is already in others
+                                ids = []
+                                for r in range(self.N):
+                                        if r != n:
+                                                ids.append(self.Replicas[r][nextIndex])
+                                                pass
+                                        pass
+                                while snew in ids:
+                                        snew = (snew+1)%self.S
+                                        pass
+
+                                        
+                                self.addToReplica(snew, n, nextIndex)
                         pass
-                        start = False
 
                 pass
 
-        # Add snew to a single Replica n, at point ri, start indicates whether we're coming from addToReplicas or not
-        def addToReplica(self, n, snew, ri, start):
-                if snew != self.Replicas[n][ri].serverId:
+        # Add the serverId snew to replica n, at point i
+        #       If it clashes at point i in other replicas:
+        #               Use the next snew (wrapped-around)
+        def addToReplica(self, snew, n, i):
 
-                        if not self.Replicas[n][ri].assigned:
-                                if start:
-                                        self.Replicas[n][ri].serverId = snew
-                                        self.Replicas[n][ri].assigned = True
-                                else:
-                                        self.Replicas[n][ri].serverId = snew
-                                        self.Replicas[n][ri].assigned = False
-                                        pass
-                                pass
-
+                self.Replicas[n][i] = snew
                 pass
 
         # Run add() x times, with serverIds [nextS, nextS+1, .. x-1]
         def addX(self, x):
+                # Readjust the number of servers
                 nextS = self.S
                 self.S += x
+
+                # Readjust the number of replicas, and fill then accordingly,
+                # with the new values
+                self.N = self.S
+                if self.N > 3:
+                        self.N = 3
+                        pass
+                self.Replicas = self.fillReplicas(self.N)
+
+                
+                # Start adding to replicas
                 for i in range(nextS, nextS+x):
                         self.addToReplicas(i)
                         pass
@@ -136,21 +141,20 @@ class DHash():
 
 
 
-
+# 
 dhash = DHash()
-dhash.addX(10)
+dhash.addX(2)
 # dhash.addX(5)
 # dhash.addX(5)
 # dhash.addX(5)
 # dhash.addX(5)
 # print dhash.update()
+
 i = 0
 for Replica in dhash.Replicas:
         print "N"+str(i)+":"
-        x = 0
         printstr = ""
         for s in Replica:
-                printstr += str(s.serverId)+" "+str(s.assigned)+", "
-                x += 1
+                printstr += str(s)+", "
         print "["+printstr[0:-2]+"]"
         i += 1
